4a4bdcc28f0ad965204aa9a4d294fd51696d510b
kevin ocansey <kevinocansey11@gmail.com>
2023-01-06 08:59:51 +0000

    completed 02-doubly-linked-list.js

--- implementation/02-doubly-linked-list.js
+++ implementation/02-doubly-linked-list.js
@@ -20,47 +20,116 @@ class DoublyLinkedList {
 
         // Add node of val to head of linked list
         let newNode = new DoublyLinkedNode(val);
+        if(!this.head && !this.tail){
+            this.tail = newNode
+            this.head = newNode
+            this.length++
+        }else if(this.length === 1){
+            this.tail.prev =  newNode
+            this.head = newNode
+            newNode.next = this.tail
+            this.length++
+        }else{
 
-        if (this.length >= 0) {
-            this.head.previous = newNode;
-            newNode.next = this.head;
-            this.head = newNode;
-        } else {
-            this.head = newNode;
-            this.tail = newNode;
+        let oldHead = this.head
+
+        newNode.next = this.head
+        this.head.prev = newNode
+        this.head = newNode
+
+        this.length++
+
+        return oldHead
         }
-
-        this.length++;
+        // O(1) head and tail values prevents traversing, hence reduce time complexity to O(1)
     }
 
     addToTail(val) {
         // Add node of val to tail of linked list
+        const newNode = new DoublyLinkedNode(val)
+        if(this.length === 0){
+            this.tail = newNode
+            this.head = newNode
+            this.length++
+        }else if(this.length === 1){
+            this.head.next = newNode
+            newNode.prev =  this.head
+            this.tail = newNode
+            this.length++
+        }else{
+        newNode.prev = this.tail
+        this.tail.next = newNode
+        this.tail = newNode
+
+        this.length++
+        }
 
         // Write your hypothesis on the time complexity of this method here
+        // O(1) head and tail values prevents traversing, hence reduce time complexity to O(1)
     }
 
     removeFromHead() {
         // Remove node at head
-
+        if( this.length === 0){
+            return undefined
+        }
+        if(this.length === 1){
+            let removed = this.head.value
+            this.head = null
+            this.length = 0
+            return removed
+        }
+        else{
+            let removed = this.head.value
+            this.head = this.head.next
+            this.head.prev = null
+            this.length--
+            return removed
+        }
         // Write your hypothesis on the time complexity of this method here
+        // O(1) head and tail values prevents traversing, hence reduce time complexity to O(1)
     }
 
     removeFromTail() {
         // Remove node at tail
+        if( this.length === 0){
+            return undefined
+        }
+        if(this.length === 1){
+            let removed = this.tail.value
+            this.tail = null
+            this.length = 0
+            return removed
+        }
+        else{
+            let removed = this.tail.value
+            this.tail = this.tail.prev
+            this.tail.next = null
+            this.length--
+            return removed
+        }
 
         // Write your hypothesis on the time complexity of this method here
+        // O(1) head and tail values prevents traversing, hence reduce time complexity to O(1)
     }
 
     peekAtHead() {
         // Return value of head node
+        if(this.head){
+            return this.head.value
+        }
 
         // Write your hypothesis on the time complexity of this method here
+        // O(1)
     }
 
     peekAtTail() {
         // Return value of tail node
-        
+        if(this.tail){
+            return this.tail.value
+        }
         // Write your hypothesis on the time complexity of this method here
+        // O(1) because tail, is stored in memory. and the computer will just need to retrieve the value directly instead of traversing the list
     }
 }
 
